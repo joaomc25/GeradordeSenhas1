d67814acdfa541c3bec3c28f69e1071f0e562db1
joaomc25 <carneiropedro2000@gmail.com>
2024-11-14 10:38:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("generateButton").addEventListener("click", generatePassword);
+
+function generatePassword() {
+  const length = 12;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~";
+  let password = "";
+  
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  
+  document.getElementById("password").value = password;
+}
